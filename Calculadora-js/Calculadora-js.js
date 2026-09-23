b0dae7b1623a26957b84610fa6ae33a3c2413dbc
Alexander Wences <alexwences13@gmail.com>
2026-09-22 22:19:41 -0600

    Se agrega nueva practica Calculadora-js

---
+++ Calculadora-js/Calculadora-js.js
@@ -0,0 +1,17 @@
+// Nota pegue el codigo en un archivo de java script para que sea más facil copiarlo y ejecutarlo en chrome.
+
+let nombre = prompt("¿Cuál es tu nombre?");
+console.log("Hola, " + nombre);
+
+let numero1 = Number(prompt("Ingresa el primer número:"));
+let numero2 = Number(prompt("Ingresa el segundo número:"));
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+console.log("Suma: " + suma);
+console.log("Resta: " + resta);
+console.log("Multiplicación: " + multiplicacion);
+console.log("División: " + division);
